d432bb93b94af9b56b6afe287691f4522619ece8
OnkarParmar <onkar.parmar@gmail.com>
2021-05-06 17:13:58 +0530

    endpoint

--- src/main/java/com/teamteach/files/controllers/S3Controller.java
+++ src/main/java/com/teamteach/files/controllers/S3Controller.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("s3")
+@RequestMapping("files")
 public class S3Controller {
 
 	@Autowired
