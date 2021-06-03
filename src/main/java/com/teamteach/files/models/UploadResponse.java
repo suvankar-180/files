dabceb15f9e2deb26513268c6bbc0f98b88b6e3e
OnkarParmar <onkar.parmar@gmail.com>
2021-06-03 12:22:55 +0530

    fix

--- src/main/java/com/teamteach/files/models/UploadResponse.java
+++ src/main/java/com/teamteach/files/models/UploadResponse.java
@@ -1,16 +1,10 @@
 package com.teamteach.files.models;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class UploadResponse {
-    private String s3Url;
-
-    public UploadResponse(String url){
-        this.s3Url = url;
-    }
-    
+    private String s3Url;    
 }
